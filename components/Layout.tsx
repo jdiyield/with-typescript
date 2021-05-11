b94a8e4ea9074516c9e8eccda16a317a260ed2ea
pullptong <hatsanai@hot-now.com>
2021-05-11 20:05:23 +0700

    style: add favicon

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react';
 
 import Head from 'next/head';
+import { DefaultSeo } from 'next-seo';
 
 type Props = {
   children?: ReactNode;
@@ -13,10 +14,22 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans"></link>
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet" />
+      <link rel="shortcut icon" href="/favicon.ico" />
     </Head>
+    <HeadSEO></HeadSEO>
     {children}
   </div>
 );
+
+const HeadSEO = () => {
+  return (
+    <DefaultSeo
+      title="JDIYIELD | One stop SAFU service for yield farmers"
+      additionalLinkTags={[{ rel: 'icon', href: `/images/logo.svg` }]}
+    />
+  );
+};
 
 export default Layout;
